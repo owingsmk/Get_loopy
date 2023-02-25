0498e128c3d68418e31a4c13485da7ad12b90e6d
Mark Owings <owingsmk@mail.uc.edu>
2023-02-25 12:43:01 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ctr = 0; //starting variable
+
+        while (ctr <= 30) {
+            System.out.println(ctr);
+            ctr = ctr + 1;
+        }
+    }
+}
